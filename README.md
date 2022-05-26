7b630dbad3ddf31ceb42772d28a1abd40b99c6e3
salvadorserruya <salvador.serruya@kavak.com>
2022-05-26 20:53:29 -0300

    procfile setup heroku

---
+++ README.md
@@ -0,0 +1,4 @@
+# nucba-ecommerce-12
+
+SETUP GIT REMOTE HEROKU
+heroku git:remote -a nucba-ecommerce-12
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api", (req, res, next) => {
+  res.send("Hello api");
+});
+app.use("/*", express.static(path.join(__dirname + "../client/build")));
+
+app.listen(PORT);
